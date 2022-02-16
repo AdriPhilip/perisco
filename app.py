6d2f9100fdda7a36a11f799fddf4bdb47e599577
AdriPhilip29 <46717583+AdriPhilip@users.noreply.github.com>
2022-02-16 17:13:22 +0100

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ mysql = MySQL()
 
 # MySQL configurations
 app.config['MYSQL_DATABASE_USER'] = 'root'
-app.config['MYSQL_DATABASE_PASSWORD'] = 'The11legende'
+app.config['MYSQL_DATABASE_PASSWORD'] = ''
 app.config['MYSQL_DATABASE_DB'] = 'perisco'
 app.config['MYSQL_DATABASE_HOST'] = 'localhost'
 app.config['MYSQL_DATABASE_PORT'] = 3306
